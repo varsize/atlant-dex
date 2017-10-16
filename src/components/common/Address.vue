066c3fddb394882739d07a9ccc014d010cb34a32
varsize <varsize@gmail.com>
2017-10-16 10:47:14 +0300

    start sidebar

---
+++ src/components/common/Address.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+.address 14rMGBspAtEv4oNdbKsKYAT7tbSgUstzBs
+</template>
+
+<script>
+import Icon from 'components/common/Icon';
+
+export default {
+  components: {
+    Icon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~variables";
+.address{
+}
+</style>
