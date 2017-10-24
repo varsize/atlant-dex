80812cfbae925474d0da07154c72a90afdecd962
DESKTOP-D9ELAA2\Paul <pashafromrussia63@gmail.com>
2017-10-24 22:11:06 +0300

    add highcharts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,46 +1,82 @@
 <template lang='pug'>
-.chart
-  canvas#myChart
+div.chart
+  div#chart
 </template>
 
 <script>
-import Chart from 'chart.js';
+import Highstock from 'highcharts/highstock';
 
 export default {
   data() {
     return {
-      myChart: {},
+      chart: {},
     };
   },
+  methods: {
+  },
   mounted() {
-    let ctx = document.getElementById('myChart');
-    this.myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-        }],
+    Highstock.stockChart('chart', {
+      chart: {
+        height: '100%',
+        spacing: [20, 40, 0, 20],
       },
-      options: {
-        responsive: false,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-            },
-          }],
+      credits: {enabled: false},
+      navigator: {enabled: false},
+      rangeSelector: {enabled: false},
+      scrollbar: {enabled: false},
+      title: '',
+      tooltip: {
+        crosshairs: {
+          color: 'green',
+          dashStyle: 'solid',
         },
+        shared: true,
+      },
+      xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: {day: '%d %b %Y'},
+      },
+      yAxis: [
+        {
+          allowDecimals: true,
+          opposite: false,
+          title: {text: ''},
+          labels: {align: 'left'},
+          lineWidth: 0,
+          gridLineWidth: 1,
+        },
+        {
+          allowDecimals: true,
+          opposite: true,
+          title: {text: ''},
+          labels: {
+            align: 'right',
+            x: 30,
+          },
+          offset: 0,
+          lineWidth: 0,
+          gridLineWidth: 1,
+        },
+      ],
+      legend: {enabled: false},
+      colors: ['#046fc4', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
+      series: [{
+        name: 'Price',
+        data: [[1, 3, 10, 1, 5], [2, 6, 12, 1, 4]],
+        type: 'candlestick',
+        yAxis: 1,
+        zIndex: 2,
+        pointPadding: 0.05,
+      },
+      {
+        name: 'Volume',
+        data: [[1, 0.5], [2, 2]],
+        type: 'column',
+        yAxis: 0,
+        zIndex: 1,
+        pointPadding: 0.05,
+      }],
+      responsive: {
       },
     });
   },
@@ -48,10 +84,8 @@ export default {
 </script>
 
 <style lang='scss'>
-.chart {
-  width: 100%;
-},
-#myChart {
-  width: 100%;
-}
+  .chart {
+    height: 100%;
+    width: 100%;
+  }
 </style>
