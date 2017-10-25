ffd58a76496de69edf84ef2cf0fae15b139c731b
Erik <erikbitner@gmail.com>
2017-10-25 21:13:49 +0300

    Design for charts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -15,57 +15,94 @@ export default {
   methods: {
   },
   mounted() {
+    let data = [[1, 3, 10, 1, 5], [2, 6, 12, 1, 4]];
     Highstock.stockChart('chart', {
       chart: {
-        height: '100%',
-        spacing: [20, 40, 0, 20],
-        reflow: false,
+        spacing: [50, 0, 0, 0],
+        reflow: true, // Whether to reflow the chart to fit the width of the container div on resizing the window. DEFAUL 'true'
+        backgroundColor: '#03354f',
+        style: {
+          fontFamily: 'Roboto',
+          fontSize: '12px',
+        },
+
       },
       credits: {enabled: false},
       navigator: {enabled: false},
-      rangeSelector: {enabled: false},
+      rangeSelector: {enabled: false}, // tool for selecting ranges to display within the chart
       scrollbar: {enabled: false},
       title: '',
       tooltip: {
         crosshairs: {
-          color: 'green',
-          dashStyle: 'solid',
+          color: '#42b6f6',
+          width: 1,
+          dashStyle: 'LongDash',
         },
         shared: true,
       },
       xAxis: {
         type: 'datetime',
         dateTimeLabelFormats: {day: '%d %b %Y'},
-      },
-      yAxis: [
-        {
-          allowDecimals: true,
-          opposite: false,
-          title: {text: ''},
-          labels: {align: 'left'},
-          lineWidth: 0,
-          gridLineWidth: 1,
-        },
-        {
-          allowDecimals: true,
-          opposite: true,
-          title: {text: ''},
-          labels: {
-            align: 'right',
-            x: 30,
+        lineColor: '#2b5072',
+        labels: {
+          align: 'center',
+          y: 30,
+          style: {
+            color: '#376691',
+            fontSize: '12px',
           },
-          offset: 0,
-          lineWidth: 0,
-          gridLineWidth: 1,
         },
-      ],
+      },
+      yAxis: [{
+        allowDecimals: true,
+        opposite: true,
+        title: {text: ''},
+        gridLineColor: '#2b5072',
+        gridLineWidth: 2, // border height
+        labels: {
+          align: 'right',
+          x: -10,
+          y: -10,
+          style: {
+            color: '#376691',
+            fontSize: 12,
+          },
+        },
+        offset: 0,
+        lineWidth: 0,
+      },
+      {
+        allowDecimals: true,
+        opposite: true,
+        title: {text: ''},
+        gridLineColor: '#2b5072',
+        gridLineWidth: 2, // border height
+        labels: {
+          align: 'right',
+          x: -10,
+          y: -10,
+          style: {
+            color: '#376691',
+            fontSize: 12,
+          },
+        },
+        offset: 0,
+        lineWidth: 0,
+      }],
+      plotOptions: {
+         candlestick: {
+            upColor: '#00ce7d',
+            upLineColor: '#00ce7d',
+            color: '#e55541',
+            lineColor: '#e55541',
+            lineWidth: 2,
+          },
+      },
       legend: {enabled: false},
-      colors: ['#046fc4', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
       series: [{
         name: 'Price',
-        data: [[1, 3, 10, 1, 5], [2, 6, 12, 1, 4]],
+        data: data,
         type: 'candlestick',
-        yAxis: 1,
         zIndex: 2,
         pointPadding: 0.05,
       },
@@ -74,8 +111,14 @@ export default {
         data: [[1, 0.5], [2, 2]],
         type: 'column',
         yAxis: 0,
+        color: '#376691',
         zIndex: 1,
         pointPadding: 0.05,
+      },
+      {
+        // type: 'column',
+        yAxis: 0,
+
       }],
       responsive: {
       },
