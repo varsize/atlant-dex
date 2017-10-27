dbad53c6637bcaf9248401062cf0d4c9b1bf139a
Eugene <es@fin-tex.com>
2017-10-27 17:59:38 +0700

    Api tweaks.

--- src/services/api/trade.js
+++ src/services/api/trade.js
@@ -1,3 +1,3 @@
 import api from './api';
 
-export const getChart = ({period, pair}) => api.get('/trade/chart', {params: {period: period, pair: pair}});
+export const getChart = ({period, pair}) => api.get('/trade/chart', {params: {period, pair}});
